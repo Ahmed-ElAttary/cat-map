7bc50a49b9387e22588ca616b0f7ce0ce8cae60c
Ahmed-ElAttary <ahmedelattary@gmail.com>
2024-03-05 16:02:55 +0200

    adding custom marker

--- components/HomeControls/CurrentPositionBtn/CurrentPositionBtn.jsx
+++ components/HomeControls/CurrentPositionBtn/CurrentPositionBtn.jsx
@@ -6,6 +6,8 @@ import Point from "ol/geom/Point.js";
 import MapContext from "@components/MapComponent/MapContext";
 import VectorSource from "ol/source/Vector";
 import VectorLayer from "ol/layer/Vector";
+import Style from "ol/style/Style";
+import Icon from "ol/style/Icon";
 
 const CurrentPositionBtn = () => {
   const { map } = useContext(MapContext);
@@ -21,9 +23,22 @@ const CurrentPositionBtn = () => {
     }
   }
   const setPoint = (position) => {
-    const vectorSource = new VectorSource({
-      features: [new Feature(new Point(position))],
+    const iconFeature1 = new Feature(new Point(position));
+    const iconFeature = new Feature(new Point(position));
+    const iconStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 1],
+        src: "map-marker-icon.png",
+        scale: 0.2,
+      }),
     });
+
+    iconFeature.setStyle(iconStyle);
+
+    const vectorSource = new VectorSource({
+      features: [iconFeature, iconFeature1],
+    });
+
     map.getView().fit(vectorSource.getExtent(), { maxZoom: 20 });
     map.removeLayer(vlRef.current);
     vlRef.current = new VectorLayer({
